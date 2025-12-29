08706c988530c0826d25ae17d623fdccff59eb62
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-12-29 14:04:03 +0800

    fix: Fixed filesystem entries using default browser agent css for links

--- controllers/filesystemController.js
+++ controllers/filesystemController.js
@@ -12,10 +12,13 @@ async function getFilesystemBase(req, res) {
 
 async function getFilesystem(req, res) {
   const fs = await db.getFolderContents(req.user.id);
+  const basedir = req.params?.folder ? req.params.folder.join("/") : "";
+  const dir = `/filesystem/${req.user.username}/${basedir}`;
 
   const props = {
     user: req.user,
     dest: req.params?.folder,
+    directory: dir,
     files: fs?.files,
     folders: fs?.folders,
   };
